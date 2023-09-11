d86849e799a21efe5ae32f8a81cb31de8e9080b6
juledek <Jule.dekyvere@gmail.com>
2023-09-11 11:40:59 +0200

    add footer + change buttons

--- port-fol/src/App.js
+++ port-fol/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage/HomePage'
+import MailLogo from './img/Mail-Green.png'
+import LinkedinLogo from './img/LinkedinLogo.png'
 import { Introduction } from './HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> JULE DEKYVERE </h1>
-      </header>
         <Navigation />
+      </header>
         <HomePage />
+        <Footer />
     </div>
   );
 }
@@ -18,16 +20,19 @@ function App() {
 function Navigation() {
   return (
     <nav className="App-nav">
-      <button className="App-button"> Page one </button>
-      <button className="App-button"> Page two </button>
-      <button className="App-button"> Page three</button>
+      <button className="App-button"> Home </button>
+      <button className="App-button"> Projects </button>
+      <button className="App-button"> Contact me</button>
     </nav>
   )
 }
 
 function Footer() {
   return (
-    <h1>Dit is de footer</h1>
+    <div className="App-footer">
+      <a href="mailto:jule.dekyvere@gmail.com"><img className="Footer-Img" src={MailLogo} alt="Mailing logo"></img></a>
+      <a href="https://www.linkedin.com/in/jule-dekyvere/"><img className="Footer-Img" src={LinkedinLogo} alt="Logo of LinkedIn"></img></a>
+    </div>
   );
 }
 
