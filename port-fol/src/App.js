c35760340a1a95242635682261802b2dceebc2b2
juledek <Jule.dekyvere@gmail.com>
2023-09-11 10:34:20 +0200

    add navigation and Home Page

--- port-fol/src/App.js
+++ port-fol/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage/HomePage'
+import { Introduction } from './HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> JULE DEKYVERE </h1>
       </header>
+        <Navigation />
+        <HomePage />
     </div>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav className="App-nav">
+      <button className="App-button"> Page one </button>
+      <button className="App-button"> Page two </button>
+      <button className="App-button"> Page three</button>
+    </nav>
+  )
+}
+
+function Footer() {
+  return (
+    <h1>Dit is de footer</h1>
   );
 }
 
---
+++ port-fol/src/HomePage/HomePage.js
@@ -0,0 +1,36 @@
+import './HomePage.css';
+
+
+
+export function Introduction() {
+    //Component with text in it as wel as a profile picture
+    return (
+        <div>
+            <div className="Introduction">
+                <h1>Hello and welcome to my Profile Page</h1>
+            </div>
+            <div className="Profile">
+                <Profile />
+            </div>
+        </div>
+       
+    );
+
+}
+
+function Profile(){
+    return (
+        <img className="ProfilePic" 
+            src="https://art.pixilart.com/sr24f9edfb668aws3.png"
+            alt="Self Portrait in Pixel Art"
+        />
+    );
+}
+
+export default function HomePage() {
+    return (
+        <div className="HomePage">
+            <Introduction />
+        </div>
+    );
+}
